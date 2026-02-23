6803d7c3bda5264f6a6484c44871ff0053be0ce4
Aaron Huwer <aaronjhuwer@gmail.com>
2026-02-23 16:48:27 -0500

    probability mapping

--- time_history_association.py
+++ time_history_association.py
@@ -1,21 +1,96 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
+REFERENCE_FOLDER = './signal_data/reference'
+EXPERIMENTAL_FOLDER = './signal_data/experimental'
+
+def load_refs():
+    references = {}
+    with os.scandir(REFERENCE_FOLDER) as refs:
+        for ref in refs:
+            reference_data = pd.read_csv(os.path.join(REFERENCE_FOLDER, ref.name))
+            references[ref.name] = reference_data
+    return references
+
+def load_expiremental(filename):
+    return pd.read_csv(filename)
+
+def best_associations(experimental, references):
+    # find the delta between every point within the expirement
+    experimental_length = len(experimental)
+
+    best_fits = {}
+
+    for reference in references:
+        reference_length = len(references[reference])
+        best_difference = None
+        best_start = 0
+
+        for start in range(reference_length - experimental_length):
+
+            difference = 0
+            for index in range(experimental_length):
+                difference += abs(references[reference]['Intensity'][start + index] - experimental['Intensity'][index])
+                
+            if(best_difference is None):
+                best_difference = difference
+                best_start = start
+
+            if(difference < best_difference):
+                best_difference = difference
+                best_start = start
+
+        best_fits[reference] = {
+            'best_difference': best_difference,
+            'best_start': best_start
+        }
+    
+    return best_fits
+
+def plot_best_fit(experimental, references, best_fits):    
+    for fit in best_fits:
+        reference = references[fit]
+        offset = best_fits[fit]["best_start"]
+
+        plt.plot(reference["Time"], reference["Intensity"], label='reference')
+        plt.plot(experimental["Time"] + offset, experimental["Intensity"], label='experiment')
+
+        plt.show()
+
+def compute_prob(best_fits):
+    differences = []
+    total_difference = 0
+    for fit in best_fits:
+        total_difference += best_fits[fit]["best_difference"]
+        differences.append((fit, best_fits[fit]["best_difference"]))
+    
+    probs = [ (fit, d / total_difference) for fit, d in differences]
+
+    probs_sorted = sorted(probs, key=lambda x: x[1])
+
+    best_fit, best_prob = probs_sorted[1]
+    second_fit, second_prob = probs_sorted[2]
+
+    best_prob = 1 - best_prob
+    second_prob = 1 - second_prob
+    
+
+    return best_fit, best_prob, second_fit, second_prob
 
 def main():
-    data_test0 = pd.read_csv('./signal_data/reference/ref_signal0.csv')
-    plt.plot(data_test0['Time'], data_test0['Intensity'], label='Reference Signal 0')
-    data_test1 = pd.read_csv('./signal_data/reference/ref_signal1.csv')
-    plt.plot(data_test1['Time'], data_test1['Intensity'], label='Reference Signal 1')
-    data_test2 = pd.read_csv('./signal_data/reference/ref_signal2.csv')
-    plt.plot(data_test2['Time'], data_test2['Intensity'], label='Reference Signal 2')
-    data_test3 = pd.read_csv('./signal_data/reference/ref_signal3.csv')
-    plt.plot(data_test3['Time'], data_test3['Intensity'], label='Reference Signal 3')
-    plt.xlabel('Time')
-    plt.ylabel('Intensity')
-    plt.title('Reference Signals')
-    plt.legend()
-    plt.show()
+    references = load_refs()
+
+    with os.scandir(EXPERIMENTAL_FOLDER) as experiments:
+        for experiment in experiments:
+            full_path = os.path.join(EXPERIMENTAL_FOLDER, experiment.name)
+            
+            experimental_data = load_expiremental(full_path)
+            best_fits = best_associations(experimental_data, references)
+            # plot_best_fit(experimental_data, references, best_fits)
+            best_fit, best_prob, second_fit, second_prob = compute_prob(best_fits)
+            print(experiment.name, best_fit, best_prob, second_fit, second_prob)
+
 
 if __name__ == "__main__":
     main()
