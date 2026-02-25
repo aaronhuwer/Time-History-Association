fbb529e8a23f8762c19a9bfcc6826f424adaf042
Aaron Huwer <aaronjhuwer@gmail.com>
2026-02-25 10:35:10 -0500

    typing and cleanup

--- time_history_association.py
+++ time_history_association.py
@@ -1,53 +1,56 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+from pathlib import Path
 
-REFERENCE_FOLDER = './signal_data/reference'
-EXPERIMENTAL_FOLDER = './signal_data/experimental'
-OUTPUT_FILE = './result.csv'
+REFERENCE_FOLDER = Path('./signal_data/reference')
+EXPERIMENTAL_FOLDER = Path('./signal_data/experimental')
+OUTPUT_FILE = Path('./result.csv')
 
-def load_refs():
+def load_refs() -> dict[str, pd.DataFrame]:
+    """Load the reference data from the reference folder into a dict of name, data"""
     references = {}
-    with os.scandir(REFERENCE_FOLDER) as refs:
-        for ref in refs:
-            reference_data = pd.read_csv(os.path.join(REFERENCE_FOLDER, ref.name))
-            references[ref.name] = reference_data
+    for ref in REFERENCE_FOLDER.iterdir():
+        reference_data = pd.read_csv(ref)
+        references[ref.name] = reference_data
     return references
 
-def load_experimental(filename):
+def load_experimental(filename: Path) -> pd.DataFrame:
+    """Load the experimental data into a dataframe"""
     return pd.read_csv(filename)
 
-def best_associations(experimental, references):
-    experimental_values = np.array(experimental["Intensity"])
+def best_associations(experimental: pd.DataFrame, references: dict[str, pd.DataFrame]) -> dict[str, dict[str, float]]:
+    """Returns the best alignments of experimental data to the references"""
+    experimental_values = experimental["Intensity"].to_numpy()
     experimental_length = len(experimental_values)
-    best_fits = {}
+    best_matches = {}
     
     for reference in references:
-        best_difference = float('inf')
+        best_error = float('inf')
         best_start = 0
-        reference_values = np.array(references[reference]["Intensity"])
+        reference_values = references[reference]["Intensity"].to_numpy()
 
-        for start in range(len(reference_values) - experimental_length):
+        for start in range(len(reference_values) - experimental_length + 1):
             
             # calculate difference using RSS
             reference_window = reference_values[start : start + experimental_length]
-            difference = np.sum(np.square(reference_window - experimental_values))
+            error = np.sum(np.square(reference_window - experimental_values))
 
-            if(difference < best_difference):
-                best_difference = difference
+            if(error < best_error):
+                best_error = error
                 best_start = start
 
-        best_fits[reference] = {
-            'best_difference': best_difference,
+        best_matches[reference] = {
+            'best_error': best_error,
             'best_start': best_start
         }
-    return best_fits
+    return best_matches
 
-def plot_best_fit(experimental, references, best_fits):    
-    for fit in best_fits:
+def plot_best_fit(experimental: pd.DataFrame, references: dict[str, pd.DataFrame], best_matches: dict[str, dict[str, float]]):
+    """Optional plotting to help visualize the best alignment between the experiment and the references"""    
+    for fit in best_matches:
         reference = references[fit]
-        offset = best_fits[fit]['best_start']
+        offset = best_matches[fit]['best_start']
 
         plt.plot(reference['Time'], reference['Intensity'], label='reference')
         plt.plot(experimental['Time'] + offset, experimental['Intensity'], label='experiment')
@@ -55,28 +58,30 @@ def plot_best_fit(experimental, references, best_fits):
 
         plt.show()
 
-def compute_prob(best_fits):
-    differences = []
-    total_difference = 0
+def compute_prob(best_matches: dict[str, dict[str, float]]) -> tuple[str, float, str, float]:
+    """Computes the probability of each match"""
 
-    for fit in best_fits:
-        total_difference += best_fits[fit]['best_difference']
-        differences.append((fit, best_fits[fit]['best_difference']))
+    # Inverse best_error so that lower error gives a higher number
+    errors = {}
+    for match in best_matches:
+        errors[match] = 1 / best_matches[match]['best_error']
     
-    probs = [ (fit, d / total_difference) for fit, d in differences]
-
-    probs_sorted = sorted(probs, key=lambda x: x[1])
-
-    best_fit, best_prob = probs_sorted[0]
-    second_fit, second_prob = probs_sorted[1]
-
-    best_prob = 1 - best_prob
-    second_prob = 1 - second_prob
+    # Normalize values to a probability
+    total = sum(errors.values())
+    probs = {}
+    for error in errors:
+        probs[error] = errors[error] / total
     
-
+    # Sort by highest probability
+    sorted_probs = sorted(probs.items(), key=lambda x: x[1], reverse=True)
+    best_fit, best_prob = sorted_probs[0]
+    second_fit, second_prob = sorted_probs[1]
+    
+    # Return two best probabilities
     return best_fit, best_prob, second_fit, second_prob
 
-def write_to_csv(name, best_fit, best_prob, second_fit, second_prob):
+def write_to_csv(name: str, best_fit: str, best_prob: float, second_fit: str, second_prob: float):
+    """Append one resulting row to the output CSV"""
     df = pd.DataFrame({
         'Signal': [name],
         'Best signal fit': [best_fit],
@@ -88,24 +93,23 @@ def write_to_csv(name, best_fit, best_prob, second_fit, second_prob):
     df.to_csv(OUTPUT_FILE, index=False, mode='a', header=False)
 
 def initialize_csv():
+    """Initialize the output CSV with only the header"""
     df = pd.DataFrame(columns=[ 'Signal', 'Best signal fit', 'Best probability', 'Second best fit', 'Second best probability' ]) 
 
     df.to_csv(OUTPUT_FILE, index=False)
 
 def main():
+    """Runs the main loop over all experiments in the folder"""
     references = load_refs()
     initialize_csv()
 
-    with os.scandir(EXPERIMENTAL_FOLDER) as experiments:
-        for experiment in experiments:
-            full_path = os.path.join(EXPERIMENTAL_FOLDER, experiment.name)            
-            experimental_data = load_experimental(full_path)
-            
-            best_fits = best_associations(experimental_data, references)
-            best_fit, best_prob, second_fit, second_prob = compute_prob(best_fits)
-            # plot_best_fit(experimental_data, references, best_fits)
+    for experiment in EXPERIMENTAL_FOLDER.iterdir():         
+        experimental_data = load_experimental(experiment)
+        
+        best_matches = best_associations(experimental_data, references)
+        best_fit, best_prob, second_fit, second_prob = compute_prob(best_matches)
 
-            write_to_csv(experiment.name, best_fit, best_prob, second_fit, second_prob)
+        write_to_csv(experiment.name, best_fit, best_prob, second_fit, second_prob)
 
 
 if __name__ == '__main__':
